08982fc12de78dd0052d6cd9aa5be47de1f88b8c
Domenic Virgilio <dvirgilio@uwm.com>
2022-06-02 12:56:11 -0400

    End of Challenge 4

--- Vigilantes.DaprWorkshop.VirtualBarista/csharp/Controllers/VirtualBaristaController.cs
+++ Vigilantes.DaprWorkshop.VirtualBarista/csharp/Controllers/VirtualBaristaController.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Vigilantes.DaprWorkshop.VirtualBarista.Models;
 namespace Vigilantes.DaprWorkshop.VirtualBarista.Controllers
 {
     [ApiController]
     public class VirtualBaristaController : ControllerBase
     {
-        public static readonly string StoreId = Environment.GetEnvironmentVariable("STORE_ID") ?? "Redmond";
+        public static readonly string StoreId = "1"; //Environment.GetEnvironmentVariable("STORE_ID") ?? "Redmond";
         public static readonly int MinSecondsToMakeDrink = int.Parse(Environment.GetEnvironmentVariable("MIN_SECONDS_TO_MAKE_DRINK") ?? "3");
         public static readonly int MaxSecondsToMakeDrink = int.Parse(Environment.GetEnvironmentVariable("MAX_SECONDS_TO_MAKE_DRINK") ?? "10");
         private static readonly object DrinkLock = new object();
@@ -36,7 +38,7 @@ namespace Vigilantes.DaprWorkshop.VirtualBarista.Controllers
                 _logger.LogInformation($"The VirtualBarista ({StoreId}) is checking orders on the make line...");
 
                 var orders = await GetOrders();
-
+         
                 _logger.LogInformation($"There {(orders.Count == 1 ? "is" : "are")} {orders.Count} {(orders.Count == 1 ? "order" : "orders")} waiting to be made.");
 
                 if (orders.Count > 0)
@@ -90,14 +92,29 @@ namespace Vigilantes.DaprWorkshop.VirtualBarista.Controllers
 
         private async Task<List<OrderSummary>> GetOrders()
         {
-            // TODO: Challenge 4 - Call the Makeline service via Dapr to get orders
+            List<OrderSummary> orderItemSummaries = null;
+
+            var response = await _httpClient.GetAsync($"http://127.0.0.1:5580/v1.0/invoke/make-line-service/method/orders/{StoreId}");
+            if(response.StatusCode != HttpStatusCode.NoContent && response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    orderItemSummaries = JsonConvert.DeserializeObject<List<OrderSummary>>(await response.Content.ReadAsStringAsync());
+                }
+                catch
+                {
+                    _logger.LogInformation("Deserialize failed: {@X}", await response.Content.ReadAsStringAsync());
+                }
+            }
             await Task.Delay(5000);
-            return Enumerable.Empty<OrderSummary>().ToList();
+            
+            return orderItemSummaries ?? new List<OrderSummary>();
         }
 
         private async Task CompleteOrder(OrderSummary orderSummary)
         {
-            // TODO: Challenge 4 - Call the Makeline service via Dapr to complete the order
+            var response = await _httpClient.DeleteAsync($"http://127.0.0.1:5580/v1.0/invoke/make-line-service/method/orders/{StoreId}/{orderSummary.OrderId}");
+            response.EnsureSuccessStatusCode();
             await Task.Delay(5000);
         }
     }
